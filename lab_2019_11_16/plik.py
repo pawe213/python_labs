afa6967c0141070017e0617a8f9cb6dfe00915ee
pawe213 <pawe213@gmail.com>
2019-11-16 07:56:48 +0100

    start2

---
+++ lab_2019_11_16/plik.py
@@ -0,0 +1,12 @@
+try:
+  f=open("text.txt","r",encoding="utf-8")
+  for line in f:
+    print(line)
+  # end
+  f.close()
+except:
+  print("problem!")
+# end
+
+
+
---
+++ lab_2019_11_16/waga.py
@@ -0,0 +1,11 @@
+def waga(li,n,p):
+    if n==0: return True
+    if p==len(li): return False
+    return waga(li,n-li[p],p+1) or waga(li,n,p+1)
+# end def
+
+odw = [1,2,5,10,16,24]
+
+
+    
+waga(odw,2,3)
